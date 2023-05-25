8e22552b655bcc782b27af6a3c6a1fd7c1e7d474
LingGrani <luthfiangger2510@gmail.com>
2023-05-25 19:55:42 +0700

    ascending order

--- Doublelinkedlist_137/Doublelinkedlist_137.cpp
+++ Doublelinkedlist_137/Doublelinkedlist_137.cpp
@@ -110,6 +110,19 @@ bool Doublelinkedlist::listEmpty() {
 	return (START == NULL);
 };
 
+void Doublelinkedlist::traverse() {
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else{
+		cout << "\nRecords in ascending order of roll number are" << endl;
+		Node* currentNode = START;
+		while (currentNode!= NULL){
+			cout << currentNode->noMhs << "" << currentNode->Nama << endl;
+			currentNode = currentNode->next;
+		}
+	}
+};
+
 int main()
 {
     std::cout << "Hello World!\n";
